a74e1fb578099724218b9fbcd10cf2a12df1c962
frontendtitan <frontendtitan@gmail.com>
2024-04-30 19:57:48 +0300

    Created basic app to test Electron React Boilerplate

---
+++ src/renderer/App.tsx
@@ -0,0 +1,20 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  )
+}
---
+++ src/renderer/index.tsx
@@ -0,0 +1,20 @@
+import { ElectronHandler } from '../main/preload'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+root.render(<App />)
+
+declare global {
+  interface Window {
+    electron: ElectronHandler
+  }
+}
+
+// calling IPC exposed from preload script
+window.electron.ipcRenderer.once('ipc-example', (arg) => {
+  // eslint-disable-next-line no-console
+  console.log(arg)
+})
+window.electron.ipcRenderer.sendMessage('ipc-example', ['ping'])
