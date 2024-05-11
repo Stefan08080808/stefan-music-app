7d4b53b1c9f1bff5666cc23e928baf5f9c0e4996
frontendtitan <frontendtitan@gmail.com>
2024-05-11 17:21:35 +0300

    Cleaned up code, moved functions into /util folder, moved selectedTab to ../App.tsx

--- src/renderer/interface/nav.tsx
+++ src/renderer/interface/nav.tsx
@@ -1,61 +1,36 @@
 import { useState, useEffect } from 'react'
+
 import NavButton, { Tabs } from './navButton'
+
 import '../style/nav.css'
 
-export default function Nav() {
+import updateTimeUtil from './util/updateTimeUtil'
+import navScrollUtil from './util/navScrollUtil'
+
+export default function Nav(props: { selectedTab: Tabs, setSelectedTab: (tab: Tabs) => void }) {
     // Time Logic
-    const [time, setTime] = useState('00:00')
-
-    const updateTime = () => {
-        const date = new Date()
-
-        let hour = String(date.getHours())
-        let min = String(date.getMinutes())
-
-        if (Number(hour) < 10) hour = `0${hour}`
-        if (Number(min) < 10) min = `0${min}`
-
-        return setTime(`${hour}:${min}`)
-    }
-
-    setInterval(updateTime, 1000)
+    const [time, setTime] = useState<string>('00:00')
+    setInterval(() => setTime(updateTimeUtil), 1000)
 
     // Tab Changing Logic
-    const [selectedTab, setSelectedTab] = useState<Tabs>('Menu')
     const [isHoveringOnNav, setIsHoveringOnNav] = useState(false)
-
     const tabs: Tabs[] = ['Menu', 'Audio', 'Online', 'Video', 'System']
 
     const changeTab = (value: Tabs) => {
-        if (value === selectedTab) return
-        setSelectedTab(value)
-        console.log(selectedTab)
+        if (value === props.selectedTab) return
+        props.setSelectedTab(value)
     }
 
     useEffect(() => {
-        const handleScroll = (event: WheelEvent) => {
-            if (event.deltaY > 0) { // Scrolling down, select next tab
-                const currentIndex = tabs.indexOf(selectedTab)
-                if (currentIndex < tabs.length - 1) {
-                    setSelectedTab(tabs[currentIndex + 1])
-                }
-            } else { // Scrolling up, select previous tab
-                const currentIndex = tabs.indexOf(selectedTab)
-                if (currentIndex > 0) {
-                    setSelectedTab(tabs[currentIndex - 1])
-                }
-            }
-
-            console.log(selectedTab)
+        const handleScrollEvent = (event: WheelEvent) => {
+            navScrollUtil(event, { selectedTab: props.selectedTab, setSelectedTab: props.setSelectedTab, tabs: tabs })
         }
 
-        if (isHoveringOnNav)
-            document.body.addEventListener('wheel', handleScroll)
-        else
-            document.body.removeEventListener('wheel', handleScroll)
+        if (isHoveringOnNav) document.body.addEventListener('wheel', handleScrollEvent)
+        else document.body.removeEventListener('wheel', handleScrollEvent)
 
-        return () => document.body.removeEventListener('wheel', handleScroll)
-    }, [selectedTab, isHoveringOnNav])
+        return () => document.body.removeEventListener('wheel', handleScrollEvent)
+    }, [props.selectedTab, isHoveringOnNav])
 
     // Application Window Logic
     const closeApp = () => window.electron.ipcRenderer.sendMessage('closeApp')
@@ -63,20 +38,27 @@ export default function Nav() {
 
     return (
         <div id='nav'>
+            {/* Top Section */}
             <div id='top'>
+
+                {/* Time Section */}
                 <div id='timeContainer'>
                     <p id='time'>{time}</p>
                 </div>
+
+                {/* Exit Button Section */}
                 <p id='exitButton' onClick={minimiseApp}>_</p>
                 <p id='exitButton' onClick={closeApp}>X</p>
             </div>
+
+            {/* Bottom Section */}
             <div id='bottom' onMouseOver={() => setIsHoveringOnNav(true)} onMouseLeave={() => setIsHoveringOnNav(false)}>
                 {tabs.map(tab => (
                     <NavButton
                         key={tab}
                         tabName={tab}
                         value={tab}
-                        selectedTab={selectedTab}
+                        selectedTab={props.selectedTab}
                         onSelectTab={changeTab}
                     />
                 ))}
