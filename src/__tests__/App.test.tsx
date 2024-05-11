4341f7cd3fdfa465bbe44e1aa28fb5fc5de90dd1
frontendtitan <frontendtitan@gmail.com>
2024-05-11 17:41:08 +0300

    Removed All Tests, I don't see the purpose

--- src/__tests__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import App from '../renderer/App'
-
-test('Render App', () => {
-    const { container } = render(<App />)
-    expect(container).toBeInTheDocument()
-})
